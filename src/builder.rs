22ad1fc8c97525b108b446ffe03414cf31a56e8a
Sean Glover <s.glover12@gmail.com>
2024-01-10 12:35:52 -0500

    cargo tests pass

--- src/builder.rs
+++ src/builder.rs
@@ -81,12 +81,13 @@ impl<S: CardShuffler> GameDealer<S> {
 impl<S: CardShuffler> Dealer for GameDealer<S> {
     fn deal(&self, player_count: usize) -> Deal {
         let evaluator = Evaluator::new();
-        let cards = RandomCardShuffler.shuffle();
+        let cards = self.shuffler.shuffle();
         let mut hands: Vec<PlayerHand> = Vec::new();
         let mut nextn = IndexGenerator::new();
         let mut i = 0;
         let players: Vec<_> = (0..player_count).collect();
         let pl = players.len();
+        println!("players: {:?}", players);
         for p in players {
             i = nextn.next().unwrap();
             let player_hand: Vec<String> = vec![cards[i].to_string(), cards[i + pl].to_string()];
@@ -137,85 +138,47 @@ impl<S: CardShuffler> Dealer for GameDealer<S> {
     }
 }
 
-
-
-fn build_deal_payload(player_count: usize, ) -> Deal {
-    let evaluator = Evaluator::new();
-    let cards = RandomCardShuffler.shuffle();
-    let mut hands: Vec<PlayerHand> = Vec::new();
-    let mut nextn = IndexGenerator::new();
-    let mut i = 0;
-    let players: Vec<_> = (0..player_count).collect();
-    let pl = players.len();
-    for p in players {
-        i = nextn.next().unwrap();
-        let player_hand: Vec<String> = vec![cards[i].to_string(), cards[i + pl].to_string()];
-        let scores: Vec<_> = player_hand.iter().map(|card| Card::new(card)).collect();
-        hands.push(PlayerHand {
-            hand: player_hand,
-            score: scores,
-        });
-    }
-
-    let flop = vec![
-        cards[nextn.next().unwrap() + pl].to_string(),
-        cards[nextn.next().unwrap() + pl].to_string(),
-        cards[nextn.next().unwrap() + pl].to_string(),
-    ];
-
-    let flop_score: Vec<u32> = flop.iter().map(|card| Card::new(card.as_str())).collect();
-    let turn = cards[nextn.next().unwrap() + pl];
-    let turn_score = Card::new(turn);
-    let river = cards[nextn.next().unwrap() + pl];
-    let river_score = Card::new(river);
-    let mut player_hands: Vec<Hand> = Vec::new();
-    for hand in hands {
-        let mut combined_score = flop_score.clone();
-        combined_score.push(turn_score);
-        combined_score.push(river_score);
-        let hand_score = evaluator.evaluate(combined_score, hand.score);
-        let score = evaluator.get_rank_class(hand_score);
-        let percentage = 1.0 - evaluator.get_five_card_rank_percentage(hand_score);
-        let description = evaluator.class_to_string(score.unwrap());
-        player_hands.push(Hand {
-            cards: hand.hand.clone(),
-            score: percentage,
-            description: description,
-        });
-    }
-    let board = Board {
-        flop: flop.clone(),
-        turn: turn.to_string(),
-        river: river.to_string(),
-    };
-    let hr = Deal {
-        board: board,
-        hands: player_hands,
-    };
-    println!("{:?}", hr);
-    hr
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct MockCardShuffler;
+
+    impl CardShuffler for MockCardShuffler {
+        fn shuffle(&self) -> Vec<&'static str> {
+            CARDS.to_vec()
+        }
+    }
+
     #[test]
-    fn test_build_deal_payload() {
-        // Test case 1: player_count = 2
-        let result = build_deal_payload(2);
-        assert_eq!(result.hands.len(), 2); // Ensure the correct number of hands are generated
-        assert_eq!(result.board.flop.len(), 3); // Ensure the correct number of flop cards are generated
-        assert_eq!(result.board.turn.len(), 1); // Ensure the correct number of turn cards are generated
-        assert_eq!(result.board.river.len(), 1); // Ensure the correct number of river cards are generated
-
-        // Test case 2: player_count = 4
-        let result = build_deal_payload(4);
-        assert_eq!(result.hands.len(), 4);
-        assert_eq!(result.board.flop.len(), 3);
-        assert_eq!(result.board.turn.len(), 1);
-        assert_eq!(result.board.river.len(), 1);
-
-        // Add more test cases as needed
+    fn test_deal() {
+        let expected_deal = Deal {
+            board: Board {
+                flop: vec!["2h".to_string(), "2s".to_string(), "3c".to_string()],
+                turn: "3d".to_string(),
+                river: "3h".to_string(),
+            },
+            hands: vec![
+                Hand {
+                    cards: vec!["Ac".to_string(), "As".to_string()],
+                    score: 0.9584561779683731,
+                    description: "Full House".to_string(),
+                },
+                Hand {
+                    cards: vec!["Ad".to_string(), "2c".to_string()],
+                    score: 0.9584561779683731,
+                    description: "Full House".to_string(),
+                },
+                Hand {
+                    cards: vec!["Ah".to_string(), "2d".to_string()],
+                    score: 0.9584561779683731,
+                    description: "Full House".to_string(),
+                },
+            ],
+        };
+        let mock_shuffler = MockCardShuffler;
+        let dealer = GameDealer::new(mock_shuffler);
+        let deal = dealer.deal(3);
+        assert!(deal == expected_deal);
     }
 }
--- src/model.rs
+++ src/model.rs
@@ -1,23 +1,23 @@
 use crate::card::Card;
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct PlayerHand {
     pub hand: Vec<String>,
     pub score: Vec<u32>,
 }
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Hand {
     pub cards: Vec<String>,
     pub score: f64,
     pub description: String,
 }
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Board {
     pub flop: Vec<String>,
     pub turn: String,
     pub river: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Deal {
     pub board: Board,
     pub hands: Vec<Hand>,
