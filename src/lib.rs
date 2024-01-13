7c4952fc0b486de1bb99e975efc5a7eae0561c0e
Sean Glover <s.glover12@gmail.com>
2024-01-13 17:17:28 -0500

    prepare to publish beta version

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+mod lookup;
+mod evaluator;
+pub mod card;
+pub mod model;
+pub mod builder;
+
+pub use builder::GameDealer;
+pub use builder::CardShuffler;
+pub use model::Deal;
+pub use model::Board;
+pub use model::Hand;
+pub use model::PlayerHand;
